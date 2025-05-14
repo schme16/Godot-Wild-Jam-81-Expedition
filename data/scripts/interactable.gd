1dbfa2a56dcbc82a0ef41c4b7f2b2e8c06133c22
Sarah <itsmad30@gmail.com>
2025-05-14 17:51:28 +1000

    Background, clouds, shark fin and a rock for level building.

--- data/scripts/interactable.gd
+++
@@ -1,9 +0,0 @@
-class_name interactable extends Node
-
-### Interaction stuff ###
-@export var is_valid_text:String
-@export var is_valid:bool
-@export var is_invalid_text:String
-
-func interaction():
-	pass
